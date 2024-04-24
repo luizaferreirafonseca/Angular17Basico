a955a4140928c912b661ab49a25d832b798ab8fd
luizaferreirafonseca <luiza.ferreira.fonseca@gmail.com>
2024-04-24 07:18:03 -0300

    fix/add adjust names and add footer

--- src/app/students-page/students-page.component.ts
+++ src/app/students-page/students-page.component.ts
@@ -4,37 +4,34 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 import { Student } from '../Models/Aluno';
 import { AveragePipe } from '../pipe/average.pipe';
 import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-students-page',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule, AveragePipe, HeaderComponent],
+  imports: [CommonModule, ReactiveFormsModule, AveragePipe, HeaderComponent, FooterComponent],
   templateUrl: './students-page.component.html',
   styleUrl: './students-page.component.css'
 })
 export class StudentsPageComponent {
 
-  getStudents: boolean = false; 
-  updateButton: boolean = false; 
+  getStudents: boolean = false;
+  updateButton: boolean = false;
   removeButton: boolean = false;
-  selectButton: boolean = true; 
+  selectButton: boolean = true;
 
   form = new FormGroup({
 
     name: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-ZÀ-ú ]+$'), Validators.maxLength(100), Validators.minLength(3)]),
     gradeOne: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(10)]),
-    gradeTwo: new FormControl(0 , [Validators.required, Validators.min(0), Validators.max(10)])
+    gradeTwo: new FormControl(0, [Validators.required, Validators.min(0), Validators.max(10)])
   })
 
 
-  cards:any = [ 'Gerenciar Estudante','Gerenciar Professor', 'Lançamento de Notas','Rotina Classe', 'Biblioteca',  'Quadro de Avisos','Controle Financeiro']
-   
+  cards: any = ['Gerenciar Estudante', 'Gerenciar Professor', 'Lançamento de Notas', 'Rotina Classe', 'Biblioteca', 'Quadro de Avisos', 'Controle Financeiro']
 
 
-
-
-
-  list:Student[] = [{
+  list: Student[] = [{
     name: 'Luiza',
     gradeOne: 10,
     gradeTwo: 10,
@@ -55,13 +52,13 @@ export class StudentsPageComponent {
     gradeTwo: 8
   }
 
-];
+  ];
 
-  indice: number = -1; 
+  indice: number = -1;
 
-  
-  registerStudent():void{
-    
+
+  registerStudent(): void {
+
     this.updateButton = false;
     this.removeButton = false;
     this.selectButton = true;
@@ -70,52 +67,52 @@ export class StudentsPageComponent {
   }
 
 
-  backToRegister():void{
+  backToRegister(): void {
     this.form.reset();
 
     this.registerStudent();
   }
 
 
-   getStudent(indice:number){
-     this.indice = indice;
-     this.form.setValue({
+  getStudent(indice: number) {
+    this.indice = indice;
+    this.form.setValue({
       name: this.list[indice].name,
       gradeOne: this.list[indice].gradeOne,
-       gradeTwo: this.list[indice].gradeTwo
-     });
+      gradeTwo: this.list[indice].gradeTwo
+    });
 
-     this.updateButton = true; 
-     this.removeButton = true;
-     this.selectButton = false;
-    
-    }
-  
- updateStudent(){
+    this.updateButton = true;
+    this.removeButton = true;
+    this.selectButton = false;
 
-this.list[this.indice] = this.form.value as Student;
+  }
 
+  updateStudent() {
 
-this.form.reset();
+    this.list[this.indice] = this.form.value as Student;
 
 
- }
-
- removeStudent(){
-  this.list.splice(this.indice,1);
-this.form.reset();
-this.updateButton = false; 
-     this.removeButton = false;
-     this.selectButton = true;
-
- }
+    this.form.reset();
 
 
- 
+  }
 
- getAllStudents(){
-  this.getStudents = true; 
- }
+  removeStudent() {
+    this.list.splice(this.indice, 1);
+    this.form.reset();
+    this.updateButton = false;
+    this.removeButton = false;
+    this.selectButton = true;
+
+  }
+
+
+
+
+  getAllStudents() {
+    this.getStudents = true;
+  }
 
 
 }
